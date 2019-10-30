7cd188c2ee08d7535d697b6d4b921daf7a9955b7
yadaDarrell <darrellshen9@gmail.com>
2019-10-30 12:59:30 +0800

    Tidying data showcasing split string tips

---
+++ tidy_data_tips_(ebola).py
@@ -0,0 +1,21 @@
+import pandas as pd 
+
+##Tidying data 
+#Reference from Daniel Chen Pydata(2018)
+
+'''
+Let's see how we can make the dataset less 'wide' but 'long'?
+We see that there are many repeats of similar records.
+Can we somehow change this to record date,day,country, deaths clearly?
+'''
+
+ebolas_data = pd.read_csv('country_timeseries.csv')
+
+ebola_long = pd.melt(ebolas_data,id_vars=['Date','Day'],)
+ebola_long.head()
+
+'''
+Holding Date and Day results in something we would want. 
+But we realise that the countries count,death lumped together 
+'''
+
